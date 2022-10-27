d47b72b747e3348c787e10a856f66a9b2aecf69f
Soubhik Banerjee <90599826+soubhik-111@users.noreply.github.com>
2022-10-27 10:46:55 +0530

    Add files via upload

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,25 +1,31 @@
 // BINARY SEARCH
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-int n,ub,lb,mid,s,a[100],flag=0;
-printf("Enter how many elements do you want to take");
-scanf("%d",&n);
-printf("Enter the elements");
-for(i=0;i<n;i++)
-printf("%d",&a[i]);
-printf("Enter the searching element");
-scanf("%d",&s);
-lb=0;
-ub=n-1;
-while(lb<ub)
-{
-if(a[mid]=s)
-{
-flag=0;
-break;
-}
-if(a[mid]>s)
-printf("")
-}
+    int i,n, ub, lb, mid, s, a[100],flag=0;
+    printf("Enter how many elements do you want to take : \n");
+    scanf("%d", &n);
+    printf("Enter the elements : \n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    printf("Enter the searching element : \n");
+    scanf("%d", &s);
+    lb = 0;
+    ub = n - 1;
+    while (lb <= ub)
+    {
+        mid=(ub+lb)/2;
+        if (a[mid] == s){
+            flag=1;
+            break;
+        }
+        else if (a[mid] > s)
+            ub=mid-1;
+        else
+            lb=mid+1;
+    }
+    if(flag==1)
+        printf("%d is present in %d position.\n",s,mid+1);
+    else
+        printf("%d not found.\n",s);
 }
