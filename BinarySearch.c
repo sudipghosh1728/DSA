14a4afe861639fb17e34a6db5c26bf4691298ab7
sudipghosh1728 <115257913+sudipghosh1728@users.noreply.github.com>
2022-10-26 21:02:34 -0700

    Create BinarySearch.c

---
+++ BinarySearch.c
@@ -0,0 +1,25 @@
+// BINARY SEARCH
+#include<stdio.h>
+void main()
+{
+int n,ub,lb,mid,s,a[100],flag=0;
+printf("Enter how many elements do you want to take");
+scanf("%d",&n);
+printf("Enter the elements");
+for(i=0;i<n;i++)
+printf("%d",&a[i]);
+printf("Enter the searching element");
+scanf("%d",&s);
+lb=0;
+ub=n-1;
+while(lb<ub)
+{
+if(a[mid]=s)
+{
+flag=0;
+break;
+}
+if(a[mid]>s)
+printf("")
+}
+}
