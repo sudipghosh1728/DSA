fcae53d5a44c400b65130b3c93a8fd44e52a6f2e
sudipghosh1728 <115257913+sudipghosh1728@users.noreply.github.com>
2022-10-30 22:01:41 +0530

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,25 +1,33 @@
-// BINARY SEARCH
+//BINARY SEARCH
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
-int n,ub,lb,mid,s,a[100],flag=0;
-printf("Enter how many elements do you want to take");
+int ar[100],i,beg=0,n,end,mid,item,flag=0;
+clrscr();
+printf("How many elements do you want:\n");
 scanf("%d",&n);
-printf("Enter the elements");
+printf("Enter the elements:\n");
 for(i=0;i<n;i++)
-printf("%d",&a[i]);
-printf("Enter the searching element");
-scanf("%d",&s);
-lb=0;
-ub=n-1;
-while(lb<ub)
+scanf("%d",&ar[i]);
+end=n-1;
+printf("Enter the elements to be searched:\n");
+scanf("%d",&item);
+while(beg<=end)
 {
-if(a[mid]=s)
+mid=(beg+end)/2;
+if(ar[mid]==item)
 {
-flag=0;
+printf("found & position=%d",mid+1);
+flag=1;
 break;
 }
-if(a[mid]>s)
-printf("")
+if(item>ar[mid])
+beg=mid+1;
+if(item<ar[mid])
+end=mid-1;
 }
+if(flag==0)
+printf("Not found");
+getch();
 }
